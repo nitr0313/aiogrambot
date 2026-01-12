460b52f4bc50c2ffb3abb986a228b6dd1e1fc16e
AndrewK <nitro313andrew@gmail.com>
2026-01-12 15:09:27 +0300

    Добавлен новый файл create_bot.py с инициализацией бота и диспетчера, удалены ненужные импорты и переменные из main.py

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 import asyncio
 import logging
 import sys
-from dotenv import load_dotenv
-from os import getenv
 
-from aiogram import Bot, Dispatcher, html, F
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-
+from create_bot import bot, dp
 from handlers.handlers import user
 
 from handlers.game_handlers import wordle
 
 
-load_dotenv()
-TELEBOT_TOKEN = getenv("TELEBOT_TOKEN")
-
-
 async def main():
-    bot = Bot(token=TELEBOT_TOKEN,
-              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp = Dispatcher()
     dp.include_routers(wordle, user)
     await dp.start_polling(bot)
 
